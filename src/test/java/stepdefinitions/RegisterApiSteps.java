0578530411352de13d05ac13fef5e15cdb7c720e
Ozge Meva Yılmaz <ozgemeva@hotmail.com>
2025-10-16 12:49:39 +0200

    api test creat account

--- src/test/java/stepdefinitions/RegisterApiSteps.java
+++ src/test/java/stepdefinitions/RegisterApiSteps.java
@@ -3,11 +3,7 @@ package stepdefinitions;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
-
-import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import TestDataSets.TestData;
 import io.cucumber.java.en.And;
@@ -15,7 +11,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
-import api.*;
+import static io.restassured.RestAssured.*;
 
 public class RegisterApiSteps {
 	String register_endpoint;
@@ -23,7 +19,8 @@ public class RegisterApiSteps {
 	String body;
 	int response_code;
 	String response_status;
-	
+	String uniqueEmail;
+
 	@Given("the API endpoint is {string}")
 	public void setEndpoint(String url) {
 		URI uri = URI.create(TestData.BASE_URL_API);
@@ -33,31 +30,28 @@ public class RegisterApiSteps {
 
 	@When("I send a POST request with valid registration data")
 	public void i_send_a_post_request_with_valid_user_registration_data() {
-		// Read json files
 
-		try {
-			body = new String(Files.readAllBytes(Paths.get("src/test/resources/testDataSet/registerUser.json")));
-			System.out.println("create body request");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		// Dynamic email
-		body = body.replace("testApi@mail.com", "testApi" + System.currentTimeMillis() + "@mail.com");
-
-		// Send Api request
-		try {
-			System.out.println("send post request");
-			response = ApiHelper.sendPostRequest(register_endpoint, body);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("Response body: ");
+		uniqueEmail = "testApi" + System.currentTimeMillis() + "@mail.com";
+		System.out.println("Dynamic email: " + uniqueEmail);
+		response = registerFormField(uniqueEmail);
+	
+		System.out.println("create body request and Response body: ");
 		response.prettyPrint();
 
 		assertNotNull(response, "Response is null — API call may have failed!");
 		assertTrue(response.statusCode() > 0, "Status code is invalid!");
 
+	}
+
+	public Response registerFormField(String uniqueEmail) {
+		
+		return given().contentType("multipart/form-data").multiPart("name", "test1").multiPart("email", uniqueEmail)
+				.multiPart("password", "123456").multiPart("title", "Mrs").multiPart("birth_date", "13")
+				.multiPart("birth_month", "10").multiPart("birth_year", "1994").multiPart("firstname", "Test")
+				.multiPart("lastname", "TestLastname").multiPart("company", "TestCompany")
+				.multiPart("address1", "TestAddress").multiPart("address2", "").multiPart("country", "Netherlands")
+				.multiPart("zipcode", "testZipcodeAA11").multiPart("state", "Holland").multiPart("city", "nord")
+				.multiPart("mobile_number", "0612345678").when().post(register_endpoint).then().extract().response();
 	}
 
 	@Then("the response status code should be 200")
@@ -67,13 +61,13 @@ public class RegisterApiSteps {
 		assertEquals(response_code, 200, "Status code is not as expected!");
 
 	}
-	
-	@And("the response should contain success")
-	 public void the_response_should_contain_success() {
-		
-		response_status = response.jsonPath().getString("status");
-		assertTrue(response_status.contains("user created"), "Expected success message not found in response!");
-	}
 
+	@And("the response should contain success")
+	public void the_response_should_contain_success() {
+
+		response_status = response.asString();
+		System.out.println("response_status: "+response_status);
+		assertTrue(response_status.contains("User created!"), "Expected success message not found in response!");
+	}
 
 }
--- src/test/resources/features/register_api.feature
+++ src/test/resources/features/register_api.feature
@@ -2,7 +2,7 @@
 Feature: User Registration API
  
   Scenario: Successful user registration
-    Given the API endpoint is "/createAccount"
+    Given the API endpoint is "/api/createAccount"
     When I send a POST request with valid registration data 
     Then the response status code should be 200
     And the response should contain success
